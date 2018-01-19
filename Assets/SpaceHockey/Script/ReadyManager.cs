ab3a3ff5e7e2603743ea0d99651a813c196e45ff
suto <ss.time2531@gmail.com>
2018-01-19 20:18:32 +0900

    PhotonCloudに接続からルーム入室まで

---
+++ Assets/SpaceHockey/Script/ReadyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PhotonRx;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceHockey.GameManagers
+{
+    public class ReadyManager : Photon.MonoBehaviour
+    {
+        [SerializeField] private Text connectText;
+
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings("1.0");
+
+            this.OnJoinedLobbyAsObservable()
+                .Subscribe(_ => PhotonNetwork.JoinRandomRoom());
+
+            this.OnPhotonRandomJoinFailedAsObservable()
+                .Subscribe(_ =>
+                {
+                    RoomOptions roomOptions = new RoomOptions()
+                    {
+                        MaxPlayers = 2,
+                        IsOpen = true,
+                        IsVisible = true,
+                    };
+                    PhotonNetwork.CreateRoom(null, roomOptions, null);
+                    Debug.Log("ルームを作成しました。");
+                });
+
+            this.OnJoinedRoomAsObservable()
+                .Subscribe(_ => Debug.Log("ルームに入室しました。"));
+
+            this.OnPhotonPlayerConnectedAsObservable()
+                .Subscribe(_ => Debug.Log("誰かがルームに入室しました。"));
+
+            this.UpdateAsObservable()
+                .Subscribe(_ => connectText.text = PhotonNetwork.connectionStateDetailed.ToString());
+        }
+    }
+}
+
